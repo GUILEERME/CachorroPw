303c98c2f7de8044793b40e8769b2dc26ab36af5
GUILEERME <guilhermealtomare@gmail.com>
2024-11-30 12:24:57 -0300

    Add files via upload

--- src/index.php
+++ src/index.php
@@ -1,1 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+<body>
+<?php
+include '../_inc/menu.php'
+?>
 
+<div class="conteudo-index">
+<div class="container-titulo">
+<h1 class="titulo">
+    Bem-vindo ao Doggo!
+</h1>
+</div>
+<div class="container-subtitulo">
+    <h2 class="subtitulo">Aqui no Doggo, celebramos a amizade única e incondicional que temos com nossos cachorros. Se você é um apaixonado por cães ou está procurando dicas para cuidar melhor do seu amigo peludo, você está no lugar certo! Nosso objetivo é fornecer tudo o que você precisa para garantir que seu cachorro tenha uma vida feliz, saudável e cheia de aventuras.</h2>
+</div>
+</div>
+</body>
+</html>
