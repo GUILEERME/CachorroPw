0b75781189e99a6c2722de6fbed1d6da807a6d5f
GUILEERME <guilhermealtomare@gmail.com>
2024-12-01 20:44:26 -0300

    Add files via upload

---
+++ src/racas.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+<body>
+    <?php
+    include '../_inc/menu.php'
+    ?>
+<div class="conteudo-racas">
+<div class="container-titulo">
+<h1 class="titulo">
+    Raças de cachorro
+</h1>
+</div>
+<div class="imagem-container">
+        <img src="../assets/img/1-ae08e8dd (1).png" alt="Imagem 1" class="imagem">
+        <img src="../assets/img/pessoafeliz1 (1).jpg" alt="Imagem 2" class="imagem">
+    </div>
+</div>
+
+</body>
+</html>
